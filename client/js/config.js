cef57a075b0c6250dc2bf24574c8caa20b6a68fb
Martin Andrew <martin.andrew@salesforce.com>
2018-06-20 09:38:36 +1000

    profile updates

--- client/js/config.js
+++ client/js/config.js
@@ -5,13 +5,13 @@ angular.module('nibs.config', [])
     .constant('FB_APP_ID','1618878675020266')
 
     .constant('STATUS_LABELS', [
-        'Forastero',
-        'Trinitario',
-        'Criollo'
+        'Bronze',
+        'Silver',
+        'Gold'
     ])
 
     .constant('STATUS_DESCRIPTIONS', [
-        'Forastero (For-ah-stare-oh)  is a common base to all but the finest of chocolates.  Our newest Nibs members move as fast as Forastero pods grow.',
-        'Trinitario (Trin-it-air-ee-yo)  is a hybrid combining the superior taste of the criollo bean with the resilience of the forastero bean. It’s not quite as rare as Criollo, but it’s close!',
-        'Criollo (Kree-oh-yo) is the most valued and rare type of cacao. Our highest level of Nibs members share the taste and sophistication of  select Crillolo pods.'
+        'Bronze members enjoy 10% off all Food and Beverage and facilities/services.',
+        'Silver members enjoy 20% off all Food and Beverage and facilities/services.',
+        'Gold members enjoy 30% off all Food and Beverage and facilities/services.'
     ]);
--- client/js/store-locator.js
+++ client/js/store-locator.js
@@ -35,10 +35,10 @@ angular.module('nibs.store-locator', [])
             icon = L.icon({
                 iconUrl: 'img/marker-icon.png',
                 shadowUrl: 'img/marker-shadow.png',
-                iconSize:     [38, 95], // size of the icon
-                shadowSize:   [50, 64], // size of the shadow
-                iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
-                shadowAnchor: [4, 62],  // the same for the shadow
+                iconSize:     [25, 41], // size of the icon
+                shadowSize:   [41, 41], // size of the shadow
+                iconAnchor:   [12, 40], // point of the icon which will correspond to marker's location
+                shadowAnchor: [14, 40],  // the same for the shadow
                 popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
             });
 
